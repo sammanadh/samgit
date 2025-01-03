32160fdc43e87dc27fc200800abd135722552c53
samman adhikari <sammanadh@gmail.com>
2025-01-03 22:04:27 +0100

    basic git "init" command implemented

--- src/main/java/io/github/sammanadh/Main.java
+++ src/main/java/io/github/sammanadh/Main.java
@@ -1,5 +1,6 @@
 package io.github.sammanadh;
 
+import javax.rmi.ssl.SslRMIClientSocketFactory;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,15 +8,16 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        if(args.length <= 1) {
-            System.err.println("USAGE: samgit [command]");
-        }
+        if (args.length < 1) exitGracefullyWithMessage("USAGE: samgit [command]");
 
         String command = args[0];
         String[] restArgs = Arrays.copyOfRange(args, 1, args.length);
 
         switch (command) {
             case "init":
+                if (restArgs.length < 1) {
+                    exitGracefullyWithMessage("USAGE: samgit init [path]");
+                }
                 String path = restArgs[0];
                 init(path);
                 break;
@@ -24,7 +26,28 @@ public class Main {
         }
     }
 
-    public static void init(String path) {
-        System.out.println("CREATED!!!!");
+    private static void init(String path) {
+        String[] fileNames = new String[]{"HEAD", "config", "description"};
+        String[] dirNames = new String[]{"hooks", "info", "objects", "refs"};
+
+        try {
+            String gitFolder = path + "/.git/";
+            // first generate the .git folder
+            Files.createDirectory(Path.of(gitFolder));
+
+            for (String fileName : fileNames) {
+                Files.createFile(Path.of(gitFolder + fileName));
+            }
+            for (String dirName : dirNames) {
+                Files.createDirectory(Path.of(gitFolder + dirName));
+            }
+        } catch (IOException ioex) {
+            System.err.println("[Error while initializing repo] " + ioex.getMessage());
+        }
+    }
+
+    private static void exitGracefullyWithMessage(String msg) {
+        System.err.println(msg);
+        System.exit(1);
     }
 }
